80e5530d7ea84ebd1533c16fb2034aa823756d24
Daniel Wedding <skinner.daniel2004@gmail.com>
2022-01-04 23:59:58 -0700

    Add EventsBar

---
+++ components/forum/events-bar/events-bar.js
@@ -0,0 +1,13 @@
+export default function EventsBar() {
+  return (
+    <div className="w-[35rem] h-[15rem] bg-gray-200 rounded-md p-6">
+      <div className="w-[25rem]">
+        <h1 className="text-[30px]">
+          Join your community and check out the events!
+        </h1>
+        <p className="text-[17px] mb-3">Check out our staff and user's scheduled events running.</p>
+        <button className="bg-blue-400 p-3 rounded-lg">Get Started</button>
+      </div>
+    </div>
+  );
+}
--- pages/forum.js
+++ pages/forum.js
@@ -1,9 +1,15 @@
 import NavBar from "../components/forum/nav-bar/nav-bar";
+import EventsBar from "../components/forum/events-bar/events-bar";
 
 export default function Forum() {
-    return (
-        <div>
-            <NavBar />
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <div className="flex flex-col mt-10 ml-10">
+          <EventsBar />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
