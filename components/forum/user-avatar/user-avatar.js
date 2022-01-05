379cd91892f8490a9479fc76e11d3c2bc010a6fb
Daniel Wedding <skinner.daniel2004@gmail.com>
2022-01-04 20:55:44 -0700

    delete a and b random pages and fix avatar spacing

--- components/forum/user-avatar/user-avatar.js
+++ components/forum/user-avatar/user-avatar.js
@@ -6,5 +6,6 @@ export default function UserAvatar({ user }){
     if(user)
         return <div></div>;
     else
-        return <div className="w-9 align-center flex"><Image src={avatar} /></div>
+        return <div className="w-20 h-16 px-6 flex flex justify-center items-center w-full
+        focus:text-orange-500 hover:bg-gray-100"><Image src={avatar} /></div>
 }
--- pages/a.js
+++
@@ -1,3 +0,0 @@
-export default function A() {
-  return <div className="bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 rounded focus:outline-none">a</div>
-}
